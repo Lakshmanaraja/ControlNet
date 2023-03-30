22e0aa662e2d57cc883d861b34c3e808cdd07ce0
Lakshmanaraja kannan <96323209+Lakshmanaraja@users.noreply.github.com>
2023-03-30 20:11:33 +0530

    Update logger.py

--- cldm/logger.py
+++ cldm/logger.py
@@ -74,3 +74,7 @@ class ImageLogger(Callback):
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
         if not self.disabled:
             self.log_img(pl_module, batch, batch_idx, split="train")
+            
+   def on_predict_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        if not self.disabled:
+            self.log_img(pl_module, batch, batch_idx, split="test")
